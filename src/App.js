68247ae499a0dc7dbf488bda3e1ee9c1f958e8f5
minggas <fasfora@gmail.com>
2018-07-27 00:37:55 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,60 +1,88 @@
-import './App.css';
 import React from 'react';
-import { MarkdownPreview } from 'react-marked-markdown';
-import { MarkdownInput } from 'react-marked-markdown';
+import marked from 'marked';
+import './App.css';
 
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+}
+
+marked.setOptions({
+  breaks: true,
+});
 
 class App extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: props.defaultValue ? props.defaultValue : '',
-      };
-    }
-    handleTextChange(e) {
-      this.setState({ value: e.target.value });
-      if (this.props.onTextChange) {
-        this.props.onTextChange(e.target.value);
-      }
-    }
-    clear() {
-      this.setState({ value: '' });
-    }
-    render() {
-      const {
-        placeholder,
-        className,
-        inputClassName,
-        inputId,
-        previewId,
-        previewClassName
-      } = this.props;
-      const { value } = this.state;
-
-      return (
-        <section className={ className }>
-          <div className='wrap'>
-            <h4 className='toolbar-title'>Editor</h4>
-          <MarkdownInput
-            placeholder={ placeholder }
-            onChange={ this.handleTextChange.bind(this) }
-            value={ value }
-            className={ inputClassName }
-            id={inputId}
-          />
-          </div>
-  
-          <div className='wrap pw-wrap'>
-            <h4 className='toolbar-title'>Previewer</h4>
-            <MarkdownPreview
-            value={ value }
-            className={ previewClassName }
-            id={previewId}
-          />
-          </div>
-        </section>
-      );
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue ? props.defaultValue : ""
+    };
+  }
+  handleTextChange(e) {
+    this.setState({ value: e.target.value });
+    if (this.props.onTextChange) {
+      this.props.onTextChange(e.target.value);
     }
   }
+  clear() {
+    this.setState({ value: "" });
+  }
+  render() {
+    const {
+      placeholder,
+      className,
+      inputClassName,
+      inputId,
+      previewId,
+      previewClassName
+    } = this.props;
+    const { value } = this.state;
+    const mark = { __html: marked(this.state.value,{ renderer: renderer, sanitize: true}) };
+    return (
+      <section className={className}>
+        <div className="wrap">
+          <h3 className="toolbar-title">Editor</h3>
+          <MarkdownInput
+            placeholder={placeholder}
+            onChange={this.handleTextChange.bind(this)}
+            value={value}
+            className={inputClassName}
+            id={inputId}
+          />
+        </div>
 
-  export default App;
+        <div className="wrap pw-wrap">
+          <h3 className="toolbar-title">Previewer</h3>
+          <MarkedPreview
+            value={mark}
+            className={previewClassName}
+            id={previewId}
+          />
+        </div>
+      </section>
+    );
+  }
+}
+
+const MarkdownInput = props => {
+  return (
+    <textarea
+      id={props.id}
+      onChange={props.onChange}
+      value={props.value}
+      className={props.className}
+    />
+  );
+};
+
+const MarkedPreview = props => {
+  return (
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={props.value}
+      className={props.className}
+    />
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,41 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 const param = {
-    placeholder: '',
-    className: 'previewer',
-    inputClassName: 'editor' ,
-    previewClassName: 'preview',
-    inputId: 'editor',
-    previewId: 'preview'    
-}
+    placeholder: "",
+    className: "previewer",
+    inputClassName: "editor",
+    previewClassName: "preview",
+    inputId: "editor",
+    previewId: "preview",
+    defaultValue: `
+# MARKDOWN PREVIEWER
+## A FCC project
+You can make your text **bold** or  put some *emphasis* on it.
+  
+\`<div>this is a line of code</div>\`
+  
+\`\`\`
+if(blockcode){
+  lineOfCode ++;
+}    
+\`\`\`
+
+This is a list
+* First item
+* Second item
+  
+>  
+> This is first level of blockquote
+>
+>>This is second level
+>
+> Back to the first level
+>
+
+![This is a kitten](https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=250&w=150)
+  
+Here you can find more about [marked](https://marked.js.org/ "link to marked page")`
+};
 
 ReactDOM.render(<App {...param}/>, document.getElementById('root'));
 registerServiceWorker();
