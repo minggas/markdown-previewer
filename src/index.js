5478459bcfb01f99fa17897687ccc8d16918554a
minggas <fasfora@gmail.com>
2018-08-18 20:17:56 -0300

    delete gitribbon component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import GitRibbon from "./components/GitRibbon";
 //import registerServiceWorker from './registerServiceWorker';
 
 const param = {
